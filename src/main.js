ad8b66187ec2604ec595572297f43d7358c4bbb7
guanyu.wang <guanyu.wang@mobilemd.cn>
2021-08-24 14:35:17 +0800

    feat: 增加 electron-builder/electron-is-dev

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} =require('electron')
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 680,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    
+});
